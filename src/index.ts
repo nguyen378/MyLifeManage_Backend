23df155a20074718451be15854179f0b049b103a
nguyen378 <82461544+nguyen378@users.noreply.github.com>
2024-08-20 22:41:23 +0700

    delete heroku

--- src/index.ts
+++ src/index.ts
@@ -12,13 +12,10 @@ app.use(express.json())
 // Routes
 app.use('/v1/auth', authRouter)
 
-app.post('/', (req, res) => {})
-
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-const PORT = process.env.PORT || 3000
 app.listen(3000, '0.0.0.0', () => {
     console.log('Server is running on http://localhost:' + 3000)
 })
